27b5213e96a81aae915be3fe64b2f1d3e1ed3722
Sem Radoes <semradijs@hotmail.com>
2022-10-10 22:22:54 +0200

    initial commit

---
+++ own-quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,93 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Sem Radoes
+        </q-toolbar-title>
+
+        <!-- <div>Quasar v{{ $q.version }}</div> -->
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import EssentialLink from 'components/EssentialLink.vue';
+
+const linksList = [
+  
+  {
+    title: 'Github',
+    caption: 'github.com/SemRadoes',
+    icon: 'github',
+    link: 'https://github.com/SemRadoes'
+  },
+  {
+    title: 'LinkedIn',
+    caption: 'Sem Radoes',
+    icon: 'linkedin-box',
+    link: 'https://www.linkedin.com/in/sem-radoes-873435196/'
+  },
+  {
+    title: 'Facebook',
+    caption: '@Sem Radoes',
+    icon: 'facebook',
+    link: 'https://www.facebook.com/sem.radoes'
+  }
+];
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+});
+</script>
---
+++ own-quasar-project/src/pages/projectsPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1 class="project-title">Projects</h1>
+</template>
+
+<style>
+    .project-title{
+        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+</style>
